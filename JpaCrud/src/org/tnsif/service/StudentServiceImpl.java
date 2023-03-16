47c82f91ae57336d27c12c81251c93899db6bc8b
shrush5 <shrushtiugale1@gmail.com>
2023-03-16 11:56:35 +0530

    first commit

---
+++ JpaCrud/src/org/tnsif/service/StudentServiceImpl.java
@@ -0,0 +1,49 @@
+package org.tnsif.service;
+
+import org.tnsif.dao.StudentDao;
+import org.tnsif.dao.StudentDaoImpl;
+import org.tnsif.entities.Student;
+
+public class StudentServiceImpl implements StudentService{
+
+	
+	private StudentDao dao;
+	
+	public StudentServiceImpl() {
+		dao=new StudentDaoImpl();
+	}
+
+	@Override
+	public void add(Student student) {
+		dao.beginTransaction();
+		dao.addStudent(student);
+		dao.commitTransaction();
+		
+	}
+
+	@Override
+	public void update(Student student) {
+		dao.beginTransaction();
+		dao.updateStudent(student);
+		dao.commitTransaction();
+		
+	}
+
+	@Override
+	public void delete(Student student) {
+		dao.beginTransaction();
+		dao.removeStudent(student);
+		dao.commitTransaction();
+		
+	}
+
+	@Override
+	public Student get(int rollno) {
+		
+		Student student=dao.getStudent(rollno);
+		return student;
+		
+		
+	}
+
+}
